43d9e26b8fc4ec97057c41d94d044def7fc43bcc
Rikard <rikard@mighty.se>
2023-11-18 22:12:09 +0100

    Tailwind

--- src/components/se-button.ts
+++ src/components/se-button.ts
@@ -2,22 +2,12 @@ import React from "react";
 import { createComponent } from "@lit/react";
 import { html, css, LitElement } from "lit";
 import { customElement, eventOptions, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+import { TWStyles } from "../styles.ts";
 
 @customElement("se-button")
 export class SeButton extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      padding: 25px;
-      color: var(--se-button-text-color, #000);
-    }
-
-    .button {
-      padding: 25px;
-      background: blue;
-      color: red;
-    }
-  `;
+  static styles = [css``, TWStyles];
 
   /** The button's theme variant. */
   @property({ reflect: true }) variant:
@@ -38,7 +28,21 @@ export class SeButton extends LitElement {
   }
 
   render() {
-    return html`<button @click=${this.onClickHandler} class="button">
+    return html`<button
+      class=${classMap({
+        button: true,
+        "bg-orange-500": this.variant === "default",
+        "bg-blue-500": this.variant === "primary",
+        "bg-green-500": this.variant === "success",
+        "bg-slate-500": this.variant === "neutral",
+        "bg-yellow-500": this.variant === "warning",
+        "bg-red-500": this.variant === "danger",
+        "p-4": this.size === "small",
+        "p-6": this.size === "medium",
+        "p-8": this.size === "large",
+      })}
+      @click=${this.onClickHandler}
+    >
       <slot></slot>
     </button>`;
   }
--- src/components/se-container.ts
+++ src/components/se-container.ts
@@ -2,23 +2,15 @@ import React from "react";
 import { createComponent } from "@lit/react";
 import { html, css, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
-
+import { TWStyles } from "../styles.ts";
 @customElement("se-container")
 export class SeContainer extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      padding: 25px;
-    }
-
-    .container {
-      padding: 25px;
-      background: pink;
-    }
-  `;
+  static styles = [css``, TWStyles];
 
   render() {
-    return html`<div class="container"><slot></slot></div>`;
+    return html`<div class="container bg-slate-700 text-white">
+      <slot></slot>
+    </div>`;
   }
 }
 
